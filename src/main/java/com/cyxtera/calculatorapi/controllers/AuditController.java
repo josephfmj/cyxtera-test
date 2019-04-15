4ff2bd3a245a19439e74907b8fc2aff1d650a92c
Josseph <josephfmj@gmail.com>
2019-04-14 20:00:11 -0500

    add Auditory controller

---
+++ src/main/java/com/cyxtera/calculatorapi/controllers/AuditController.java
@@ -0,0 +1,35 @@
+package com.cyxtera.calculatorapi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cyxtera.calculatorapi.mongo.model.OperationsAudit;
+import com.cyxtera.calculatorapi.mongo.repository.OperationAuditRepository;
+
+@RestController("/auditory")
+public class AuditController {
+	
+	@Autowired
+	private OperationAuditRepository operationAuditRepository;
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<OperationsAudit>> getAllOperationAudit(){
+		
+		return new ResponseEntity<>(operationAuditRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{auditoriId}")
+	public ResponseEntity<OperationsAudit> findById(@PathVariable("auditoriId") String auditoriId){
+		;
+		return operationAuditRepository.findById(auditoriId)
+				.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+
+}
